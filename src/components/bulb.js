0bda1f35af23318be90f2802359363978b00e263
erlindsalla <sallaerlind@gmail.com>
2021-08-03 17:43:41 +0200

    bulb excercise

--- src/components/bulb.js
+++ src/components/bulb.js
@@ -2,13 +2,15 @@
 import React, { useState } from "react";
 import "./bulb.css";
 
-
-<<<<<<< HEAD
-const Bulb = () => {
-    
-    
+/*
+  inside the ./components create a another component with 4x4 bulbs shaped like a square. 
+  when you click the switch only the bulbs on the odd position  should be turned on ex: 1 3 5 etc 
+  hint: use an array with objects to store the data [{state: true}]
+  then map this array 
+  no logic should be in the bulb.js component just pas the state as a prop and the onClick function 
+*/
+const Bulb = (props) => {
     const [bulbOn, setBulbOn] = useState(false);
-    
     
   return (
         <>
@@ -16,38 +18,6 @@ const Bulb = () => {
         <button onClick={() => setBulbOn(!bulbOn)}> {bulbOn ? "off" : "on"} </button>
         
    </>
-=======
-/*
-  use arrow function for class components
-
-  const Bulb = () => {
-    code goes here
-  } 
-*/
-
-// images such as .svg .jpg .png etc belong on a folder called images inside the ./public directory of the project
-function Bulb() {
-  // "on" as the name for the state is not descriptive enough use something like this bulbState 
-  // read clean code to have a better understanding on how to name variables and functions
-  const [on, setOn] = useState(false);
-
-  //redundant function
-  // just use setOn directly on the onClick event
-  /*
-      onClick={()=> setOn(!on)}
-  */
-  const lightSwitch = () => setOn(on => !on);
-
-  return (
-    <>
-      <div className={on ? "bulb-on" : "bulb-off"} />
-      {/*
-        button name should represent the state that the bulb will go into  ex: if bulb is on button name should be off 
-        
-      */}
-      <button onClick={lightSwitch}>On/off</button>
-    </>
->>>>>>> e7855e7543fd83aa226edae1aa5856b9dbcdea84
   );
 }
 
